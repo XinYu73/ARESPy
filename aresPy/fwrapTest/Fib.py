17bb990c8cca2453e4e459c4a67c4e87a20c3354
XinYu73 <xy741496743@gmail.com>
2022-03-21 16:02:08 +0800

    f

---
+++ aresPy/fwrapTest/Fib.py
@@ -0,0 +1,20 @@
+from __future__ import print_function, absolute_import, division
+import _Fib
+import f90wrap.runtime
+import logging
+
+def fib(a, n):
+    """
+    fib(a, n)
+    
+    
+    Defined at fib2.f90 lines 2-19
+    
+    Parameters
+    ----------
+    a : float
+    n : int
+    
+    """
+    _Fib.f90wrap_fib(a=a, n=n)
+
---
+++ aresPy/fwrapTest/Fib/fib2.f90
@@ -0,0 +1,16 @@
+
+      SUBROUTINE FIB(A,N)
+
+      INTEGER,INTENT(IN) ::N
+      REAL*8,intent(inout) :: A(N)
+
+      DO I=1,N
+         IF (I.EQ.1) THEN
+            A(I) = 0.0D0
+         ELSEIF (I.EQ.2) THEN
+            A(I) = 1.0D0
+         ELSE 
+            A(I) = A(I-1) + A(I-2)
+         ENDIF
+      ENDDO
+      END
