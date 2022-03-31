1376dfbdc99ef00f0f319d1d77c5117b7f7fb9eb
XinYu73 <xy741496743@gmail.com>
2022-03-31 10:34:05 +0800

    f

--- aresPy/fwrapTest/LinkAresLib/my.f90
+++ aresPy/fwrapTest/LinkAresLib/my.f90
@@ -1,9 +1,10 @@
-subroutine matrixMult(n1, A, D)
-    external EVLRG
-    INTEGER, intent(in) :: n1
-    DOUBLE PRECISION, intent(in) :: A(n1,n1)
-    DOUBLE PRECISION, intent(out) :: D(n1)
-    CALL EVLRG(A,D)
+subroutine matrixMult(n, A, D)
+     external dgesv
+    INTEGER, intent(in) :: n
+    DOUBLE PRECISION, intent(in) :: a(n,n)
+    DOUBLE PRECISION, intent(inout) :: D(n)
+    integer :: i,info,lda,ldb ,nrhs
+    integer ,dimension(n) :: ipiv
+    CALL dgesv(n,nrhs,a,ldb,ipiv,D,ldb,info)
     RETURN
-
 end subroutine matrixMult
--- aresPy/fwrapTest/LinkAresLib/test.f90
+++ aresPy/fwrapTest/LinkAresLib/test.f90
@@ -1,22 +1,23 @@
 program test
 implicit none
-external matvecA
+external dgesv
 integer,parameter :: n= 3
 real(kind=8), dimension(n) :: x,b
 real(kind=8), dimension(n,n) :: a,xx,yy
 integer :: i,info,lda,ldb ,nrhs
 integer ,dimension(n) :: ipiv
 
-a = reshape((/1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0/),(/n,n/))
+a = reshape((/1.0,45.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0/),(/n,n/))
 xx = reshape((/1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0/),(/n,n/))
 yy = reshape((/1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0/),(/n,n/))
-b=(/1.0,2.0,3.0/)
+b=(/1.5,2.0,3.0/)
 write(*,*) 'j(1) = '
 x=b
 nrhs=1
 lda=n
 ldb=n
 !call matvecA(n,a,b)
-call  matMult_loop1(a, a, a,3, 3, 3)
-write(*,*) a(1,1)
+!call  matMult_loop1(a, a, a,3, 3, 3)
+call dgesv(n,nrhs,a,ldb,ipiv,x,ldb,info)
+write(*,*) x
 end program test
